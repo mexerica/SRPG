668b5555ebb0396d45b318193bc958b120c42393
mexerica <produtivo63@gmail.com>
2021-08-20 22:01:22 -0300

    taticamente

---
+++ tatic/Assets/Resources/TacticsCamera.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TacticsCamera : MonoBehaviour
+{
+    public void RotateLeft()
+    {
+        transform.Rotate(Vector3.up, 90, Space.Self);
+    }
+
+    public void RotateRigth()
+    {
+        transform.Rotate(Vector3.up, -90, Space.Self);
+    }
+}
